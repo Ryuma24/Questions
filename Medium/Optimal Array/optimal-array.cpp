753139bd0264f032b791c935b53f4ff4a87e7ed4
Pankaj Kumar <90641333+Ryuma786@users.noreply.github.com>
2023-02-28 10:33:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Optimal Array/optimal-array.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> optimalArray(int n,vector<int> &a){
+        // Code here
+        int mid;
+        int pre_moves=0;
+        vector<int>moves;
+        for(int i=0;i<n;i++){
+            mid=i/2;
+            pre_moves+=a[i]-a[mid];
+            moves.push_back(pre_moves);
+        }
+        return moves;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        Solution obj;
+        vector<int> ans=obj.optimalArray(n,a);
+        for(auto ele:ans){
+            cout<<ele<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
